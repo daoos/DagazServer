5e325d43834763c22bbebcb642178a9680d0e5a4
GlukKazan <glukkazan@gmail.com>
2022-06-07 09:43:42 +0300

    Nubia

--- src/migration/1596108130561-game.ts
+++ src/migration/1596108130561-game.ts
@@ -277,6 +277,7 @@ export class game1596108130561 implements MigrationInterface {
         await queryRunner.query(`insert into game_variants(id, game_id, name, filename, players_total) values(240, 37, 'Andalusia', 'andalusia', 2)`);
         await queryRunner.query(`insert into game_variants(id, game_id, name, filename, players_total) values(241, 24, 'Column Checkers (10x8)', '80-cells-columns', 2)`);
         await queryRunner.query(`insert into game_variants(id, game_id, name, filename, players_total) values(242, 22, 'Lika', 'lika', 2)`);
+        await queryRunner.query(`insert into game_variants(id, game_id, name, filename, players_total) values(243, 37, 'Nubia', 'nubia', 2)`);
 
         await queryRunner.query(`insert into game_styles(id, game_id, name, suffix, player_num) values(1, 23, 'European', '', null)`);
         await queryRunner.query(`insert into game_styles(id, game_id, name, suffix, player_num) values(2, 23, 'Chinese', '-kanji', null)`);
@@ -421,6 +422,7 @@ export class game1596108130561 implements MigrationInterface {
         await queryRunner.query(`insert into game_bots(id, game_id, variant_id, selector_value, player_num) values(119, 37, 240, null, null)`);
         await queryRunner.query(`insert into game_bots(id, game_id, variant_id, selector_value, player_num) values(120, 24, 241, null, null)`);
         await queryRunner.query(`insert into game_bots(id, game_id, variant_id, selector_value, player_num) values(121, 22, 242, null, null)`);
+        await queryRunner.query(`insert into game_bots(id, game_id, variant_id, selector_value, player_num) values(122, 37, 243, null, null)`);
 
         await queryRunner.query(`insert into game_setups(id, game_id, variant_id, selector_value, name) values(1, 25, 10, 1, 'Without Handicap')`);
         await queryRunner.query(`insert into game_setups(id, game_id, variant_id, selector_value, name) values(2, 25, 10, 2, 'Left-Kyo Handicap')`);
@@ -979,6 +981,7 @@ export class game1596108130561 implements MigrationInterface {
         await queryRunner.query(`insert into game_previews(id, filename, selector_value, preview, rules, copyright) values(602, 'andalusia', null, 'andalusia', 'https://www.zillions-of-games.com/cgi-bin/zilligames/submissions.cgi?do=show;id=1733', '2009 Chris Huntoon')`);
         await queryRunner.query(`insert into game_previews(id, filename, selector_value, preview, copyright) values(603, '80-cells-columns', null, '80-cells-columns', 'Nikolay Spancireti (1916-1991)')`);
         await queryRunner.query(`insert into game_previews(id, filename, selector_value, preview) values(604, 'lika', null, 'russian-checkers-1')`);
+        await queryRunner.query(`insert into game_previews(id, filename, selector_value, preview, copyright) values(605, 'nubia', null, 'nubia', '2009 Chris Huntoon')`);
 }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
